aa3455bc55634562703da2a0ee221a2b7b86e49a
saamhassanabdi <saamhassanabdi@gmail.com>
2025-07-04 13:34:45 +0300

    Create Aamino hassan.java

---
+++ Aamino hassan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class C1230740 {
+public static void main(String[] args) {
+Scanner input=new Scanner(System.in);
+System.out.println("Enter 5 strings");
+String[] strings = new String[5];
+for (int i = 0; i < strings.length; i++) {
+strings[i] = input.nextLine();
+}
+System.out.println("Strings  uppercase");
+for (String  string: strings) {
+System.out.println(string.toUpperCase());
+}
+
+}  
+} codka inaa dhigo aan rabo waa kan
+
