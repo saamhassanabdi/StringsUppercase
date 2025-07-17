060c9ef7b97932a34a8546d584f0ac7c33f3ddfa
saamhassanabdi <saamhassanabdi@gmail.com>
2025-07-17 13:34:00 -0700

    Update Aamino hassan.java

--- Aamino hassan.java
+++ Aamino hassan.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class C1230740 {
+public class assemt2{
 public static void main(String[] args) {
 Scanner input=new Scanner(System.in);
 System.out.println("Enter 5 strings");
